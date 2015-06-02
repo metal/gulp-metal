520efd4c5b8ac07cd0415d031053a96cb024b1aa
Maira Bello <maira.araujo@liferay.com>
2015-06-02 13:48:51 -0300

    Changes global variable name to be "aui"

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function normalizeOptions(options) {
 	var codeGlobs = ['src/**/*.js', '!src/**/*.soy.js', 'test/**/*.js', 'gulpfile.js'];
 
 	options.buildDest = options.buildDest || 'build/globals';
+	options.globalName = options.globalName || 'aui';
 	options.soyGeneratedDest = options.soyGeneratedDest || 'build';
 
 	options.formatGlobs = options.formatGlobs || codeGlobs;
