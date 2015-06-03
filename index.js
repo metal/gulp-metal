29544d52beaea591f4551c176ce3d84f2442fefb
Maira Bello <maira.araujo@liferay.com>
2015-06-03 15:43:14 -0300

    Adds task for building src to a globals bundle with jquery plugins

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var file = require('gulp-file');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var metaljs = require('metaljs');
+var path = require('path');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
+var wrapper = require('gulp-wrapper');
 
 function auiTasks(options) {
 	options = normalizeOptions(options);
@@ -28,7 +30,22 @@ function auiTasks(options) {
 	});
 
 	gulp.task('build:js', function(done) {
-		runSequence('build:globals', done);
+		runSequence('soy', ['build:globals:js', 'build:globals:jquery'], done);
+	});
+
+	gulp.task('build:globals:jquery', function() {
+		return gulp.src(options.buildSrc)
+			.pipe(wrapper({
+				footer: function(file) {
+					var className = path.basename(file.path);
+					className = className.substr(0, className.length - 3);
+					var classNameLowerCase = className[0].toLowerCase() + className.substr(1);
+					return 'import JQueryAdapter from \'bower:metal-jquery-adapter/src/JQueryAdapter\';' +
+						'JQueryAdapter.register(\'' + classNameLowerCase + '\', ' + className + ')';
+				}
+			}))
+			.pipe(metaljs.buildLazyPipes.buildGlobals(options)())
+			.pipe(gulp.dest(options.buildGlobalsJqueryDest));
 	});
 
 	gulp.task('watch', function(done) { // jshint ignore:line
@@ -85,6 +102,7 @@ function normalizeOptions(options) {
 	var codeGlobs = ['src/**/*.js', '!src/**/*.soy.js', 'test/**/*.js', 'gulpfile.js'];
 
 	options.buildDest = options.buildDest || 'build/globals';
+	options.buildGlobalsJqueryDest = options.buildGlobalsJqueryDest || 'build/globals-jquery';
 	options.bundleCssFileName = options.bundleCssFileName || 'all.css';
 	options.scssSrc = options.scssSrc || 'src/**/*.scss';
 	options.skipCssBuild = !!options.skipCssBuild;
