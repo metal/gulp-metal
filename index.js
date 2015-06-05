b7b0b9dc8326176f017112217aecdc463a2ac688
Maira Bello <maira.araujo@liferay.com>
2015-06-05 17:55:50 -0300

    Exposes renameAlias fn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 var build = require('./lib/build');
 var soy = require('./lib/soy');
 var registerTasks = require('./lib/tasks/index');
+var renameAlias = require('./lib/renameAlias');
 
 module.exports.registerTasks = registerTasks;
+module.exports.renameAlias = renameAlias;
 
 module.exports.build = build;
 module.exports.soy = soy;
