da5f2cdcdedfc1ad2cd9963844ec1f1280250582
Maira Bello <maira.araujo@liferay.com>
2015-06-30 16:52:26 -0300

    Adds tests for the "css" task

--- test/lib/tasks/css.js
+++ test/lib/tasks/css.js
@@ -17,9 +17,48 @@ describe('CSS Task', function() {
 		process.chdir(this.initialCwd_);
 	});
 
+	it('should concatenate css and compiled scss files into a single bundle', function(done) {
+		registerTasks();
+
+		gulp.start('css', function() {
+			var contents = fs.readFileSync('build/all.css', 'utf8');
+			assert.notStrictEqual(-1, contents.indexOf('.foo'));
+			assert.notStrictEqual(-1, contents.indexOf('.sass'));
+			done();
+		});
+	});
+
+	it('should get css and sass files from requested src', function(done) {
+		registerTasks({
+			cssSrc: 'css/anotherPath.css',
+			scssSrc: 'css/anotherPath.scss'
+		});
+
+		gulp.start('css', function() {
+			var contents = fs.readFileSync('build/all.css', 'utf8');
+			assert.notStrictEqual(-1, contents.indexOf('.anotherPathCss'));
+			assert.notStrictEqual(-1, contents.indexOf('.anotherPathSass'));
+			done();
+		});
+	});
+
+	it('should use sass include paths indicated by the scssIncludePaths option', function(done) {
+		registerTasks({
+			scssIncludePaths: ['css'],
+			scssSrc: 'css/importer.scss'
+		});
+
+		gulp.start('css', function() {
+			var contents = fs.readFileSync('build/all.css', 'utf8');
+			assert.notStrictEqual(-1, contents.indexOf('.importer'));
+			assert.notStrictEqual(-1, contents.indexOf('.anotherPathSass'));
+			done();
+		});
+	});
+
 	it('should trigger "end" event even when task throws error for invalid sass file', function(done) {
 		registerTasks({
-			scssSrc: 'css/invalid.scss'
+			scssSrc: 'css/invalid/invalid.scss'
 		});
 		sinon.stub(console, 'error');
 
