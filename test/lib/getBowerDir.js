d9d651336d0f076f61b6b64d728ee2867ec9b5f1
Henrique Vicente <henriquevicente@gmail.com>
2015-11-22 01:55:15 -0300

    Use bower to get the bower_components directory instead of bower-directory.

--- test/lib/getBowerDir.js
+++ test/lib/getBowerDir.js
@@ -2,24 +2,11 @@
 
 var assert = require('assert');
 var rewire = require('rewire');
-var sinon = require('sinon');
 
 var getBowerDir = rewire('../../lib/getBowerDir');
-var fakeBowerDirectory = {
-	sync: sinon.stub().returns('/some/bower/components/path')
-};
 
 describe('getBowerDir', function() {
-	before(function() {
-		getBowerDir.__set__('bowerDirectory', fakeBowerDirectory);
-	});
-
-	it('should cache value of bowerDirectory.sync once and use it for all calls', function() {
-		var originalValue = fakeBowerDirectory.sync();
-		assert.strictEqual(originalValue, getBowerDir());
-
-		fakeBowerDirectory.sync.returns(originalValue + '/another/path');
-		assert.notStrictEqual(originalValue, fakeBowerDirectory.sync());
-		assert.strictEqual(originalValue, getBowerDir());
+	it('should return value of bowerDirectory', function() {
+		assert.strictEqual(getBowerDir({cwd: '/foo/bah'}), '/foo/bah/bower_components');
 	});
 });
