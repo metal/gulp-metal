ed280a8d59044b989cca5bd113d899a7f8475d93
Henrique Vicente <henriquevicente@gmail.com>
2015-12-22 11:53:36 -0300

    Fix build globals pipeline path.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var build = require('./lib/pipelines/build');
+var build = require('metal-tools-build-globals/lib/pipelines/buildGlobals');
 var registerTasks = require('./lib/tasks/index');
 
 module.exports.registerTasks = registerTasks;
