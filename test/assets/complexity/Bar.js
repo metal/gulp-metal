018b2628abfd31c81316481e4a7015599f098c84
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-12-19 17:18:23 -0800

    Add gulp complexity task for simple command line use

--- test/assets/complexity/Bar.js
+++ test/assets/complexity/Bar.js
@@ -1,6 +1,20 @@
 'use strict';
 
 class Bar {
+	complexFn(foo, bar) {
+		if (foo) {
+			if (bar) {
+			}
+			else {
+			}
+		}
+		else if (bar) {
+			if (foo) {
+			}
+			else {
+			}
+		}
+	}
 }
 
 export default Bar;
